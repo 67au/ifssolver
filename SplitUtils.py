517b07197dcd5df7d0d653fab58c05a9e78ff919
67au <oyy6745@gmail.com>
2021-02-07 23:35:38 +0800

    更换了分割图像的方案

--- SplitUtils.py
+++ SplitUtils.py
@@ -6,18 +6,26 @@ import numpy as np
 import portion as P
 
 
-def getPhotoContours(img_path, y_start=0, y_end=0, thresh=None):
+def getPhotoContours(img_path, y_start=0, y_end=0, thresh=200):
+    """
+    分割图像获取矩形轮廓（分割方法略为魔法，能用就行）
+    :param img_path: 输入分割图像的路径
+    :param y_start: 裁剪y轴方向的开头x像素
+    :param y_end: 裁剪y轴方向的结尾x像素
+    :param thresh: 像素与图像背景(50, 50, 50)的欧氏距离的平方（对于95%图像来说为200）
+    :return: 分割获得的矩形列表
+    """
     img = cv.imread(img_path)
     img_crop = img[y_start:img.shape[0] - y_end, :]
-    img_gray = cv.cvtColor(img_crop, cv.COLOR_BGR2GRAY)
-    thresh = thresh or (42, 68)
-    img_threshold = cv.inRange(img_gray, *thresh)
+    std = np.array([50, 50, 50])
+    img_temp = np.sum(np.power(img_crop - std, 2), axis=2)
+    img_threshold = cv.inRange(img_temp, 0, thresh)
     kernel = np.ones((3, 3), np.uint8)
     edge = img_threshold
     img_zero = np.zeros_like(img_threshold)
     for i in range(10):
-        edge = cv.morphologyEx(edge, cv.MORPH_OPEN, kernel=kernel, iterations=4)
-        edge = cv.morphologyEx(edge, cv.MORPH_CLOSE, kernel=kernel, iterations=4)
+        edge = cv.morphologyEx(edge, cv.MORPH_OPEN, kernel=kernel, iterations=1)
+        edge = cv.morphologyEx(edge, cv.MORPH_CLOSE, kernel=kernel, iterations=1)
         contours, _ = cv.findContours(255 - edge, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             cv.drawContours(img_zero, [cv.convexHull(cnt)], -1, 255, -1)
@@ -27,12 +35,21 @@ def getPhotoContours(img_path, y_start=0, y_end=0, thresh=None):
     for cnt in contours:
         x, y, h, w = cv.boundingRect(cnt)
         cv.rectangle(img_zero2, (x, y), (x + h, y + w), 255, -1)
+    contours, _ = cv.findContours(img_zero2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        x, y, h, w = cv.boundingRect(cnt)
+        cv.rectangle(img_zero2, (x, y), (x + h, y + w), 255, -1)
     contours, _ = cv.findContours(img_zero2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE, offset=(0, y_start))
     rect_list = [cv.boundingRect(cnt) for cnt in contours]
     return rect_list
 
 
 def getPhotoMap(rect_list):
+    """
+    将矩形轮廓排序
+    :param rect_list: 输入矩形轮廓列表
+    :return: 排序结果的二维数组
+    """
     photo_col = []
     for n, cnt in enumerate(rect_list):
         flag = True
