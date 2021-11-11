39117c9b356af6471798229343daa01a9bd0ea66
67au <oyy6745@gmail.com>
2021-11-11 14:24:13 +0800

    feat(utils): 添加背景色可选参数.
    Signed-off-by: 67au <oyy6745@gmail.com>

--- solver/utils.py
+++ solver/utils.py
@@ -11,9 +11,10 @@ def parse_portal_filename(lat: float, lng: float, image_url: str):
     return f'{lat}_{lng}_{hashlib.md5(image_url.encode()).hexdigest()}.jpg'
 
 
-def get_picture_max_border(image_path: PathType, thresh=200) -> Tuple[int, int]:
+def get_picture_max_border(image_path: PathType, thresh: int = 200,
+                           BGR_std: Tuple[int, int, int] = (50, 50, 50)) -> Tuple[int, int]:
     img = cv.imread(str(image_path))
-    std = np.array([50, 50, 50])
+    std = np.array(BGR_std)
     img_binary = cv.inRange(np.sum(np.power(img - std, 2), axis=2), 0, thresh)
     contours: List[np.ndarray]
     contours, _ = cv.findContours(255 - img_binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
